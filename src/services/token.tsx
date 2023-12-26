ad18718dcd79f358ba33a2e5bd0f41a1175713da
Andres Obando <practicante@plasticolab.com>
2023-12-26 13:21:13 -0500

    fixed key bugs

--- src/services/token.tsx
+++ src/services/token.tsx
@@ -1,5 +1,5 @@
-const apiId = 'b5fed5be54ae4099bed1fadddc87d854'
-const apiSecret = '09d958ed3373467bb67c0f050671e718'
+const apiId = import.meta.env.VITE_SPOTIFY_API_ID
+const apiSecret = import.meta.env.VITE_SPOTIFY_API_SECRET
 
 export const getToken = async () => {
     const res = await fetch('https://accounts.spotify.com/api/token', {
